5cd280fec30683a13846bd2f63cf0c2f816b5966
SzczypekJ <szczypek@student.agh.edu.pl>
2024-09-07 10:27:37 +0200

    Read and count number of occurances

--- homework3/Maven_project/src/main/java/com/solvd/mavenproject/Main.java
+++ homework3/Maven_project/src/main/java/com/solvd/mavenproject/Main.java
@@ -1,7 +1,29 @@
 package com.solvd.mavenproject;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        try {
+            String content = FileUtils.readFileToString(new File("src/test/new.txt"), StandardCharsets.UTF_8);
+
+            String[] words = StringUtils.split(content.trim(), null);
+            Map<String, Integer> wordCount = new HashMap<>();
+
+            for (String word : words) {
+                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+            }
+
+            FileUtils.writeStringToFile(new File("src/test/results.txt"), wordCount.toString(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
