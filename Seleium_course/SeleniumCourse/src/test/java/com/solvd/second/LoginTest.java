3d26120f2fc04618fd2b72191d48b545b4447b82
Jakub Sczypek <jakub.szczypek@tlen.pl>
2024-12-10 20:58:23 +0100

    Starting with selenium course - learning about webdriver

---
+++ Seleium_course/SeleniumCourse/src/test/java/com/solvd/second/LoginTest.java
@@ -0,0 +1,43 @@
+package com.solvd.second;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginTest {
+
+    public static String browser = "chrome";
+    public static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+        }
+
+        driver.get("https://www.saucedemo.com");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
